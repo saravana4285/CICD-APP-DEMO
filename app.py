79b5608c36c0b0afb1a6d1e288ae5a72e8e46d86
saravana4285 <saravana4285@gmail.com>
2023-05-04 11:31:30 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,15 @@
+#This code is written by Saravanan
+# version 2.0
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
+     
+
+
+
