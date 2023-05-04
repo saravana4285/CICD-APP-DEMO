369c28bef6c196e4e063610f838de4234e5a5d3a
saravana4285 <saravana4285@gmail.com>
2023-05-04 11:38:59 +0530

    second commit

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Hello World! - welcome to lab"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
